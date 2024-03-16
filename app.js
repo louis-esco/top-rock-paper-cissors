a32dd791945c734f408204f9e009051d53fecb12
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-16 10:53:35 +0100

    Creates function that defines the round winner

--- app.js
+++ app.js
@@ -4,3 +4,26 @@ function getComputerChoice() {
     let computerChoice = options[randNum];
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelect = playerSelection.toLocaleLowerCase();
+    let computerSelect = computerSelection.toLocaleLowerCase();
+
+    if (playerSelect === computerSelect) {          // Tie game case
+        return "It's a tie game!";
+
+    } else if (                                     // Player wins cases
+        playerSelect === "rock" && computerSelect === "cissors" ||
+        playerSelect === "paper" && computerSelect === "rock" ||
+        playerSelect === "cissors" && computerSelect === "paper") {
+        return `You win, ${playerSelect} beats ${computerSelect}!`
+
+    } else if (                                 // Computer wins cases
+        playerSelect === "rock" && computerSelect === "paper" ||
+        playerSelect === "paper" && computerSelect === "cissors" ||
+        playerSelect === "cissors" && computerSelect === "rock") {
+        return `You loose, ${computerSelect} beats ${playerSelect}!`
+    }
+}
+
+console.log(playRound('ROck', getComputerChoice()));
